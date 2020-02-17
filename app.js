d100c87ca6c762efe532c6846236950e93ba879b
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-16 20:43:43 -0700

    added get request for footnotes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Publications';
 
 app.get('/', (request, response) => {
@@ -23,6 +23,16 @@ app.get('/api/v1/papers', (request, response) => {
   database('papers').select()
     .then((papers) => {
       response.status(200).json(papers);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/footnotes', (request, response) => {
+  database('footnotes').select()
+    .then((footnotes) => {
+      response.status(200).json(footnotes);
     })
     .catch((error) => {
       response.status(500).json({ error });
