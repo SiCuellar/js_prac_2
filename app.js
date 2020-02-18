23763a754ae09eb8706ed6316dbfda32d9026895
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-17 20:18:10 -0700

    paper refactor

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('./knexfile')[environment];
+// const database = require('knex')(configuration);
+
+const Paper = require('../models/paper')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,14 +22,14 @@ app.listen(app.get('port'), () => {
 });
 
 app.get('/api/v1/papers', (request, response) => {
-  database('papers').select()
+  Paper.all()
     .then((papers) => {
       response.status(200).json(papers);
     })
     .catch((error) => {
-      response.status(500).json({ error });
-    });
-});
+      response.status(500).json({error});
+    })
+})
 
 app.get('/api/v1/footnotes', (request, response) => {
   database('footnotes').select()
